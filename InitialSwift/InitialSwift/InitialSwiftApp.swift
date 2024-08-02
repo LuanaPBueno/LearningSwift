a5caeb943deb25874c90276cd5e30daf09be8e8f
LuanaBueno <lubuenorj@gmail.com>
2024-08-02 18:06:06 -0300

    commit

---
+++ InitialSwift/InitialSwift/InitialSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  InitialSwiftApp.swift
+//  InitialSwift
+//
+//  Created by Luana Bueno on 02/08/24.
+//
+
+import SwiftUI
+
+@main
+struct InitialSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InitialSwift/InitialSwift/Variables&Constantes.swift
@@ -0,0 +1,19 @@
+//
+//  Variables.swift
+//  InitialSwift
+//
+//  Created by Luana Bueno on 02/08/24.
+//
+
+import SwiftUI
+
+//VARIÁVEL
+
+//Variáveis são valores ao longo do código que eu posso querer mudar em algum outro momento. Para isso, posso definir VAR - de variável, depois falar o tipo e no final seu valor. Exp:
+
+var idade:Int = 20 //variável (valor mutável) do tipo int, ou seja, um valor inteiro, que é igual ao valor 20.
+
+//CONSTANTE
+//Constantes são valores que não podem ser mudados mais ao longo do código após ser definido pela primeira vez. Para isso, utilizo a denominação let
+
+let nome:String = "Luana"
